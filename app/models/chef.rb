86465df6b513fdec68ad95580b11858e80079fdf
Martins <kalninsmartins1@gmail.com>
2019-05-31 20:12:47 +0100

    - Rubocop adjustments.

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,8 +1,8 @@
 class Chef < ApplicationRecord
   has_many :recipes
-  validates :name, presence: true, length: {minimum:3, maximum:20}
+  validates :name, presence: true, length: {minimum: 3, maximum: 20}
   validates :email, presence: true,
-            length: {minimum:3, maximum:320},
-            format: { with: URI::MailTo::EMAIL_REGEXP },
-            uniqueness: { case_sensitive: false }
+                    length: {minimum: 3, maximum: 320},
+                    format: {with: URI::MailTo::EMAIL_REGEXP},
+                    uniqueness: {case_sensitive: false}
 end
--- db/migrate/20190528190013_rename_chef_name_column.rb
+++ db/migrate/20190528190013_rename_chef_name_column.rb
@@ -1,3 +1,4 @@
+# Renaming Chefs table name column
 class RenameChefNameColumn < ActiveRecord::Migration[5.2]
   def change
     rename_column :chefs, :chef_name, :name
