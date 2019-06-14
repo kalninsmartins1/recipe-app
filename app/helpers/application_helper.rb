9e42e32a4bde3c94212d1c7ad5042a83d5b4b07a
Martins <kalninsmartins1@gmail.com>
2019-06-14 20:19:32 +0100

    - Add image to recipes show and index page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def image(size)
+    image_tag('http://www.sclance.com/pngs/smiley-face-transparent-png/smiley_face_transparent_png_1258029.png',
+              size: "#{size}x#{size}")
+  end
 end
